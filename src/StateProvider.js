22972490801fe9feaba4d731319c21a47486c151
Abhishek Saraswat <77572876+Abhisheksaraswat21@users.noreply.github.com>
2022-03-13 08:23:38 +0530

    Update StateProvider.js

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,8 +1,4 @@
-//stateprovider and reducer i have copied direclty and they are wxactly like redux but their implementation is easy
-//now we dont have to pass user as prop as we have the user as we have in redux
-//we can use it without passing
 
-//we have created a layer of this js file on our index.js
 
 import React, {createContext, useContext, useReducer }  from 'react' ;
 
